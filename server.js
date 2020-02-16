cc0e40a4e1cb452e983d4f91a8b996aa9b4e30f8
Mark Gacoka Mbui <23658445+markgacoka@users.noreply.github.com>
2020-02-16 08:39:31 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var https = require('https');
+var http = require("http");
 var path = require('path');
 var bodyParser = require('body-parser');
 
@@ -15,8 +15,7 @@ var config = require(path.join(__dirname, configFile));
 
 //express app
 var app = express();
-const port = 3446;
-const hostname = '127.0.0.1';
+var port = 1337;
 
 app.use(express.static(__dirname));
 
@@ -33,30 +32,20 @@ app.get('/houndifyAuth', Houndify.HoundifyExpress.createAuthenticationHandler({
 //sends the request to Houndify backend with authentication headers
 app.post('/textSearchProxy', bodyParser.text({ limit: '1mb' }), Houndify.HoundifyExpress.createTextProxyHandler());
 
-if (config.https) {
-
-  //ssl credentials
-  var privateKey = fs.readFileSync(config.sslKeyFile);
-  var certificate = fs.readFileSync(config.sslCrtFile);
-  var credentials = { key: privateKey, cert: certificate };
-
-  //https server
-  var httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(port, function() {
+http.createServer(function(req, res) {
     fs.readFile("index.html", function(err, data){
       response.writeHead(200, {'Content-Type': 'text/html'});
       response.write(data);
       response.end();
       console.log("HTTPS server running on port", port);
-      console.log("Open https://127.0.0.1:" + port, "in the browser to view the Web SDK demo");
-    });
-  });
+      console.log("Open https://localhost:" + port, "in the browser to view the Web SDK demo");
+  }).listen(1337, "localhost");
 
 } else {
 
   app.listen(port, function() {
     console.log("HTTP server running on port", port);
-    console.log("Open http://127.0.0.1:" + port, "in the browser to view the Web SDK demo");
-  });
+    console.log("Open http://localhost:" + port, "in the browser to view the Web SDK demo");
+  }).listen(1337, "localhost");
 
 }
