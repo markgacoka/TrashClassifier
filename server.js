a50ed4aacc013f78ade8a441eab57945d786d4d9
Mark Gacoka Mbui <23658445+markgacoka@users.noreply.github.com>
2020-02-16 09:32:43 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const app = require('./express/server');
-var express = require('express');
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
 var fs = require('fs');
-var https = require('https');
 var path = require('path');
 var bodyParser = require('body-parser');
 
@@ -18,47 +18,19 @@ var config = require(path.join(__dirname, configFile));
 
 //express app
 const port = 1336;
-const hostname = 'https://treehacks.netlify.com/';
+const hostname = 'localhost';
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-//authenticates requests
-app.get('/houndifyAuth', Houndify.HoundifyExpress.createAuthenticationHandler({ 
-  clientId:  config.clientId, 
-  clientKey: config.clientKey
-}));
+app.post('/textSearchProxy', (res, req) => {
+  const newValue = Houndify.HoundifyExpress.createTextProxyHandler()
+  res.body = bodyParser.text({ limit: '1mb' })
+  req.json(newValue);
+});
 
-//sends the request to Houndify backend with authentication headers
-app.post('/textSearchProxy', bodyParser.text({ limit: '1mb' }), Houndify.HoundifyExpress.createTextProxyHandler());
-
-if (config.https) {
-
-  //ssl credentials
-  var privateKey = fs.readFileSync(config.sslKeyFile);
-  var certificate = fs.readFileSync(config.sslCrtFile);
-  var credentials = { key: privateKey, cert: certificate };
-
-  //https server
-  var httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(port, function() {
-    fs.readFile("index.html", function(err, data){
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(data);
-      response.end();
-      console.log("HTTPS server running on port", port);
-      console.log("Open https://localhost:" + port, "in the browser to view the Web SDK demo");
-    });
-  });
-
-} else {
-
-  app.listen(port, function() {
-    console.log("HTTP server running on port", port);
-    console.log("Open http://localhost:" + port, "in the browser to view the Web SDK demo");
-  });
-
-}
+module.exports.handler = serverless(app);
