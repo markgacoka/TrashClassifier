30a25e9c9b7215591fdac6e5790853bb4ac19b9b
Mark Gacoka Mbui <23658445+markgacoka@users.noreply.github.com>
2020-02-16 09:59:02 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var Houndify = require('houndify');
+var Houndify = require('houndify').HoundifyExpress;;
 
 //parse arguments
 var argv = require('minimist')(process.argv.slice(2));
@@ -27,6 +27,12 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/textSearchProxy', bodyParser.text({ limit: '1mb' }), Houndify.HoundifyExpress.createTextProxyHandler());
+app.get('/houndifyAuth', houndifyExpress.createAuthenticationHandler({ 
+  clientId:  "zps_7PSMlmeNbpVdeQfi5g==",
+  clientKey: "xGW0T-wp27AT6ye6gARikAtHwpAttJw6Wo0qjezCWqrRHVjvM66YCNLRhD6WfNStA0NfJoke67kJELULzCIQTg=="
+}));
+ 
+//proxies text requests
+app.post('/textSearchProxy', bodyParser.text({ limit: '1mb' }), Houndify.createTextProxyHandler());
 
 module.exports.handler = serverless(app);
