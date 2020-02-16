3a737351224377ba823165fcdb2791b62ddef6be
Mark Gacoka Mbui <23658445+markgacoka@users.noreply.github.com>
2020-02-16 10:10:20 -0800

    Added correct files

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-'use strict';
-
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
+var express = require('express');
 var fs = require('fs');
+var https = require('https');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var Houndify = require('houndify').HoundifyExpress;;
+var Houndify = require('houndify');
 
 //parse arguments
 var argv = require('minimist')(process.argv.slice(2));
@@ -17,22 +14,49 @@ var configFile = argv.config || 'config';
 var config = require(path.join(__dirname, configFile));
 
 //express app
-const port = 3000;
-const hostname = 'localhost';
+var app = express();
+const port = 3446;
+const hostname = '127.0.0.1';
 
 app.use(express.static(__dirname));
-app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/houndifyAuth', houndifyExpress.createAuthenticationHandler({ 
-  clientId:  "zps_7PSMlmeNbpVdeQfi5g==",
-  clientKey: "xGW0T-wp27AT6ye6gARikAtHwpAttJw6Wo0qjezCWqrRHVjvM66YCNLRhD6WfNStA0NfJoke67kJELULzCIQTg=="
+//authenticates requests
+app.get('/houndifyAuth', Houndify.HoundifyExpress.createAuthenticationHandler({ 
+  clientId:  config.clientId, 
+  clientKey: config.clientKey
 }));
- 
-//proxies text requests
-app.post('/textSearchProxy', bodyParser.text({ limit: '1mb' }), Houndify.createTextProxyHandler());
 
-module.exports.handler = serverless(app);
+//sends the request to Houndify backend with authentication headers
+app.post('/textSearchProxy', bodyParser.text({ limit: '1mb' }), Houndify.HoundifyExpress.createTextProxyHandler());
+
+if (config.https) {
+
+  //ssl credentials
+  var privateKey = fs.readFileSync(config.sslKeyFile);
+  var certificate = fs.readFileSync(config.sslCrtFile);
+  var credentials = { key: privateKey, cert: certificate };
+
+  //https server
+  var httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(port, function() {
+    fs.readFile("index.html", function(err, data){
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(data);
+      response.end();
+      console.log("HTTPS server running on port", port);
+      console.log("Open https://127.0.0.1:" + port, "in the browser to view the Web SDK demo");
+    });
+  });
+
+} else {
+
+  app.listen(port, function() {
+    console.log("HTTP server running on port", port);
+    console.log("Open http://127.0.0.1:" + port, "in the browser to view the Web SDK demo");
+  });
+
+}
