6de94cf1396d5b808c68088ea917714596abe058
Mark Gacoka Mbui <23658445+markgacoka@users.noreply.github.com>
2020-02-17 09:05:10 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,62 @@
-var express = require('express');
-var fs = require('fs');
-var https = require('https');
-var path = require('path');
-var bodyParser = require('body-parser');
+var crypto = require('crypto');
+ 
+/**
+ * Given Houndify Client Id and Client Key in options objects
+ * returns an Express request handler for authenticating Voice Requests.
+ * Signs a token/message with Houndify Client Key using the HMAC scheme.
+ * The request for authentications will contain "token" query parameter
+ * that needs to be signed with secret Client Key.
+ *
+ * @param {Object} opts - Options
+ * @return {Function} An Express request handler
+ */
+function createAuthenticationHandler(opts) { 
+    return function (req, res) {
+        var clientKey = opts.clientKey.replace(/-/g, "+").replace(/_/g, "/");
+        var clientKeyBin = new Buffer(clientKey, "base64");
+        var hash = crypto.createHmac("sha256", clientKeyBin).update(req.query.token).digest("base64");
+        var signature = hash.replace(/\+/g, "-").replace(/\//g, "_");
+        res.send(signature);
+    }
+}
 
-var Houndify = require('houndify');
-
-//parse arguments
-var argv = require('minimist')(process.argv.slice(2));
-
-//config file
-var configFile = argv.config || 'config';
-var config = require(path.join(__dirname, configFile));
-
-//express app
-var app = express();
-const port = 3446;
-const hostname = '127.0.0.1';
-
-app.use(express.static(__dirname));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-//authenticates requests
-app.get('/houndifyAuth', Houndify.HoundifyExpress.createAuthenticationHandler({ 
-  clientId:  config.clientId, 
-  clientKey: config.clientKey
-}));
-
-//sends the request to Houndify backend with authentication headers
-app.post('/textSearchProxy', bodyParser.text({ limit: '1mb' }), Houndify.HoundifyExpress.createTextProxyHandler());
-
-if (config.https) {
-
-  //ssl credentials
-  var privateKey = fs.readFileSync(config.sslKeyFile);
-  var certificate = fs.readFileSync(config.sslCrtFile);
-  var credentials = { key: privateKey, cert: certificate };
-
-  //https server
-  var httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(port, function() {
-    fs.readFile("index.html", function(err, data){
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      response.write(data);
-      response.end();
-      console.log("HTTPS server running on port", port);
-      console.log("Open https://127.0.0.1:" + port, "in the browser to view the Web SDK demo");
-    });
-  });
-
-} else {
-
-  app.listen(port, function() {
-    console.log("HTTP server running on port", port);
-    console.log("Open http://127.0.0.1:" + port, "in the browser to view the Web SDK demo");
-  });
-
+var request = require('request');
+ 
+/**
+ * Returns a simple Express handler for proxying Text Requests.
+ * The handler takes query parameters and houndify headers, 
+ * and sends them in the request to backend (GET https://api.houndify.com/v1/text). 
+ *
+ * @return {Function} An Express request handler
+ */
+function createTextProxyHandler() {
+    return function (req, res) {
+        var houndifyHeaders = {};
+        for (var key in req.headers) {
+            var splitKey = key.toLowerCase().split("-");
+            if (splitKey[0] == "hound") {
+                var houndHeader = splitKey.map(function(pt) {
+                    return pt.charAt(0).toUpperCase() + pt.slice(1);
+                }).join("-");
+                houndifyHeaders[houndHeader] = req.headers[key];
+            }
+        }
+ 
+        //GET requests contain Request Info JSON in header.
+        //POST requests contain Request Info JSON in body. 
+        //Use POST proxy if Request Info JSON is expected to be bigger than header size limit of server
+        houndifyHeaders['Hound-Request-Info'] = houndifyHeaders['Hound-Request-Info'] || req.body;
+ 
+        request({
+            url: "https://api.houndify.com/v1/text",
+            qs: req.query,
+            headers: houndifyHeaders
+        }, function (err, resp, body) {
+            //if there's an request error respond with 500 and err object
+            if (err) return res.status(500).send(err.toString());
+            
+            //else send the response body from backend as it is
+            res.status(resp.statusCode).send(body);
+        });  
+    }
 }
